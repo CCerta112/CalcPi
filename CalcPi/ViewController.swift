a4d1af5a368553e366352968d84430356d91c707
CCerta112 <christian.certa@googlemail.com>
2015-02-24 12:48:07 +0100

    Initial Commit

---
+++ CalcPi/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CalcPi
+//
+//  Created by Christian Certa on 24.02.15.
+//  Copyright (c) 2015 CertaSystems. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
